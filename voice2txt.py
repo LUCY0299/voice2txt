6e6679e7c498afc4974026d2a9e3d2b347f5e201
LUCY0299 <peng1012955@gmail.com>
2023-08-29 16:06:30 +0800

    voice2txt add mp3towave method

--- voice2txt.py
+++ voice2txt.py
@@ -1,8 +1,8 @@
 import socket
-import subprocess
-import os
 import re
-from datetime import datetime
+import pyaudio
+import wave
+import subprocess
 
 import logging
 from threading import Thread, active_count
@@ -13,7 +13,7 @@ import speech_recognition as sr
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
 
 def sanitize_filename(filename):
-    # 使用正則表達式將文件名中的非法字符替換為空
+    # 使用正規表示式過濾掉不可見字符和特殊字符，只保留英數字、底線、句點和連字符
     return re.sub(r'[^\w.-]', '', filename)
 
 def audio_file_to_text(audio_file_name):
@@ -31,32 +31,84 @@ def audio_file_to_text(audio_file_name):
     return result
 
 def handle_client_connection(client_socket):
-    # 接收客戶端傳送的檔名
-    filename = client_socket.recv(1024).decode('utf-8')
-    # 對檔名進行清理，確保它只包含合法字符
-    filename = sanitize_filename(filename)       
-               
-    # 生成帶有時間戳的檔名
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M')
-    filename_with_timestamp = f"AUDIO_{timestamp}_{filename}.3gp"
+    try:# 接收客戶端傳送的檔名
+        filename = client_socket.recv(1024).decode('utf-8')
+         # 做文件名的错误处理和验证
+        if not filename.strip():
+            logging.warning("接收到空白文件名")
+            return
+        
+        filename = sanitize_filename(filename)  
+        if not filename:
+            logging.warning("無效的文件名")
+            return
+        # 對檔名進行清理，確保它只包含合法字符
+        filename = sanitize_filename(filename)       
+                
+        # # 生成帶有時間戳的檔名
+        # timestamp = datetime.now().strftime('%Y%m%d_%H%M')
+        # filename_with_timestamp = f"AUDIO_{timestamp}_{filename}.3gp"
 
-    with open(filename_with_timestamp, "wb") as f:
-        while True:
-            # 持續接收客戶端傳輸的數據並寫入檔案
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            f.write(data)
-    logging.info(f"音訊檔案已儲存為 {filename_with_timestamp}")
+        # 存放音訊檔案的路徑
+        audio_file_path = "D:/Topic/VoiceToText/" + filename
     
-    # 將音頻檔案轉換為文字
-    result = audio_file_to_text(filename_with_timestamp)
-    logging.info(f"轉換後的文字內容：{result}")
+        with open(audio_file_path, "wb") as f:
+            while True:
+                # 持續接收客戶端傳輸的數據並寫入檔案
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                f.write(data)
+        logging.info(f"音訊檔案已儲存為 {filename}")
+        
+         # 将 3GP 音频文件转换为 WAV 格式
+        wav_audio_file_path = audio_file_path.replace('.3gp', '.wav')
+        subprocess.run(['ffmpeg', '-i', audio_file_path, wav_audio_file_path])
+        
+        # 將音頻檔案轉換為文字
+        result = audio_file_to_text(audio_file_path)
+        logging.info(f"轉換後的文字內容：{result}")
+        
+        # 播放音频文件
+        play_audio(audio_file_path)
+        
+        # 将转换结果存储到文本文件中
+        store_result(result)
+        
+    except Exception as e:
+        logging.error(f"处理客户端连接时发生错误：{e}")
+    finally:
+        # 关闭客户端连接
+        client_socket.close()
     
     # 傳送回應消息給客戶端
     response = "音訊已接收、儲存並轉換為文字"
     client_socket.send(response.encode())
     client_socket.close()
+    
+# 添加播放音频的函数
+def play_audio(audio_file_path):
+    CHUNK = 1024
+    File = wave.open(audio_file_path, "rb")
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(File.getsampwidth()), 
+                    channels=File.getnchannels(), 
+                    rate=File.getframerate(), 
+                    output=True)
+    data = File.readframes(CHUNK)
+    while data:
+        stream.write(data)
+        data = File.readframes(CHUNK)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# 添加存储转换结果的函数
+def store_result(result):
+    outfile = 'D:/Topic/VoiceToText/Voiceresult.txt'
+    with open(outfile, 'a', encoding='CP950') as f:
+        f.write(result + '\n')
+    print('\n\n檔案'+outfile+'己存檔。')
 
 def start_socket_server():
     # 建立socket伺服器，綁定地址並監聽連線
